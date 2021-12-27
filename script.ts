8e51f0b5a507d5b650db19381326ca83a7e0ac3c
Cauã Rojas <cauarojas@hotmail.com>
2021-12-26 21:52:18 -0300

    adicionei a função de adicionar tarefas

--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+const list = document.querySelector('ul'),
+	todoName = document.querySelector('input'),
+	addTodoButton = document.querySelector('button');
+
+const addTodo = (text: string) => {
+	const todo = document.createElement('li');
+	todo.innerText = text;
+	list?.append(todo);
+};
